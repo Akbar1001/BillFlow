95489ba75f0640ae395b8cb874804a56251bfceb
Akbar <akbar.code20@gmail.com>
2026-06-30 16:55:47 +0530

    Testing 1st route

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def hello(request):
+    return HttpResponse("Welcome to BillFlow 🚀")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", hello),
 ]
