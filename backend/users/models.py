0c5305a2e37b514398794fd0d36a1482c2660fc1
Akbar <akbar.code20@gmail.com>
2026-07-02 21:26:52 +0530

    schema for user build successfully

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,11 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-
-    email = models.EmailField(unique=True)
-
+class User(AbstractUser):
     phone_number = models.CharField(
         max_length=15,
         blank=True
@@ -15,9 +12,5 @@ class User(models.Model):
         default=False
     )
 
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-
     def __str__(self):
-        return self.name
+        return self.username
