640dc0fcbc87458538df435fc7bd1e4d39963afc
Akbar <akbar.code20@gmail.com>
2026-07-15 15:54:02 +0530

    Invoices API working correctly

--- backend/invoices/models.py
+++ backend/invoices/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from customers.models import Customer
+from django.utils import timezone
 
 class InvoiceStatus(models.TextChoices):
     DRAFT = "DRAFT", "Draft"
@@ -18,6 +19,7 @@ class Invoice(models.Model):
     invoice_number = models.CharField(
     max_length=30,
     unique=True,
+    blank=True,
     )
 
     issue_date = models.DateField()
@@ -63,6 +65,29 @@ class Invoice(models.Model):
     updated_at = models.DateTimeField(
         auto_now=True,
     )
+
+    def save(self, *args, **kwargs):
+
+        if not self.invoice_number:
+
+            year = timezone.now().year
+
+            last_invoice = Invoice.objects.filter(
+                invoice_number__startswith=f"INV-{year}"
+            ).order_by("-id").first()
+
+            if last_invoice:
+                last_number = int(
+                    last_invoice.invoice_number.split("-")[-1]
+                ) + 1
+            else:
+                last_number = 1
+
+            self.invoice_number = (
+                f"INV-{year}-{last_number:04d}"
+            )
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.invoice_number
